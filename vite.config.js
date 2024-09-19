68c8cd5092892bed2aa3f2aa4632d80200600c43
Faiyaz <faiyaz.sumon@gmail.com>
2024-09-19 22:50:16 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Base path for assets
-  // build: {
-  //   outDir: 'dist',
-  //   rollupOptions: {
-  //     output: {
-  //       assetFileNames: 'assets/[name].[hash].[ext]',
-  //       entryFileNames: 'assets/[name].[hash].js',
-  //       chunkFileNames: 'assets/[name].[hash].js',
-  //     },
-  //   },
-  // },
-});
+})
